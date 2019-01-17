8a43733af106513b5bafd6cb43f981955eae980f
Desmond Mullen <dsm@busylittlestudios.com>
2019-01-17 10:24:10 -0500

    initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=kitten&api_key=l1WgOAzyUj9zBJQAGD6fSPdBmHLszb5w";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    let theEmbedURL = response.data[0].embed_url;
+    let theFixedWidthStill = response.data[0].images.fixed_width_still.url;
+    $("#result").html("<div><img src=\"" + theFixedWidthStill + "\" width='150px'><iframe src=\"" + theEmbedURL + "\" width='150px' frameBorder='0'></iframe></div>");
+
+    console.log(theEmbedURL);
+    console.log(theFixedWidthStill);
+});
