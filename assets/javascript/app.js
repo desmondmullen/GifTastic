37cb16543ca49be2f74f983707fccb4bdd20db85
Desmond Mullen <dsm@busylittlestudios.com>
2019-01-17 23:16:41 -0500

    beta 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     var theButtons = ['typing cats', 'piano cats', 'sleepy cats', 'sneaky cats', 'cat fails', 'cat wins', 'sleepy kittens', 'sneaky kittens', 'kitten fails', 'kitten wins'];
+    var theLastSearch = theButtons[0];
+    var theOffset = 0;
 
     function makeButton(theButton) {
-        $('#buttons').append($('<button>').attr({ id: theButton, class: 'btn btn-secondary query-button' }).text(theButton));
+        $('#buttons').append($('<button>').attr({ id: theButton, class: 'btn btn-info query-button' }).text(theButton));
     }
 
     for (let n = 0; n < theButtons.length; n++) {
@@ -10,11 +12,15 @@ $(document).ready(function () {
     };
 
     $('.query-button').click(function (event) {
-        getGifs(event.target.id);
+        getGifs(event.target.id, true);
     });
 
     $('#add-new-button').click(function () {
         makeNewButton()
+    });
+
+    $('#more-button').click(function () {
+        getGifs(theLastSearch, false);
     });
 
     $("#new-button-text").on('keypress', function (event) {
@@ -28,21 +34,30 @@ $(document).ready(function () {
         if (theButtonName !== "") {
             $('#new-button-text').val("");
             makeButton(theButtonName);
-            getGifs(theButtonName);
+            getGifs(theButtonName, true);
         }
     }
 
-    function getGifs(theGifsToGet) {
+    //retrieve 20 at a time, keep track so we can retrieve more with a more button
+
+    function getGifs(theGifsToGet, clearTheSlate) {
         theGifsToGet = theGifsToGet.split(' ').join('+'); //to take care of any spaces
         let theRating = document.querySelector("input[name='rating']:checked").value;
-        let theQuery = 'https://api.giphy.com/v1/gifs/search?q=' + theGifsToGet + '&rating=' + theRating + '&api_key=l1WgOAzyUj9zBJQAGD6fSPdBmHLszb5w';
+        if (clearTheSlate === true) {
+            theOffset = 0;
+        };
+        let theQuery = 'https://api.giphy.com/v1/gifs/search?q=' + theGifsToGet + '&rating=' + theRating + '&limit=20&offset=' + theOffset + '&api_key=l1WgOAzyUj9zBJQAGD6fSPdBmHLszb5w';
         $.ajax({
             url: theQuery,
             method: 'GET'
         }).then(function (response) {
-            $('#last-search').text('last search: ' + theGifsToGet + ', rating: ' + theRating);
-            $('#gifs-portfolio').empty();
+            theLastSearch = theGifsToGet;
+            $('#last-search').text('last search: ' + theLastSearch + ', rating: ' + theRating);
+            if (clearTheSlate === true) {
+                $('#gifs-portfolio').empty();
+            };
             for (let n = 0; n < response.data.length; n++) {
+                theOffset += response.data.length;
                 let theName = response.data[n].title;
                 let theRating = response.data[n].rating;
                 let theFixedWidthURL = response.data[n].embed_url;
@@ -51,5 +66,5 @@ $(document).ready(function () {
             }
         });
     }
-    getGifs(theButtons[0]);
+    getGifs(theLastSearch, true);
 });
